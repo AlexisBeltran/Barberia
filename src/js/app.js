43fd7563f9317d5a85f83f848903d9426cfaf450
Alexis Beltran <jesus_beltran011@outlook.com>
2021-04-17 22:53:25 -0600

    Se agrego el html de los servicios

--- src/js/app.js
+++ src/js/app.js
@@ -14,7 +14,22 @@ async function mostrarServicios(){
         const {servicios} = db;
         //Generar el HTML
         servicios.forEach(servicio => {
-            console.log(servicio);
+            const { id, nombre, precio } = servicio;
+            //Generar el nombre del producto
+            const nombreServicio = document.createElement('p');
+            nombreServicio.textContent = nombre;
+            nombreServicio.classList.add('nombre-servicio');
+            //Generar el precio del producto
+            const precioServicio = document.createElement('p');
+            precioServicio.textContent = `$ ${precio}`;
+            precioServicio.classList.add('precio-servicio');
+            //Generar el div contenedor del servicio
+            const divServicio = document.createElement('div');
+            divServicio.classList.add('servicio');
+            //Inyectar precio y nombre al div servicio
+            divServicio.appendChild(nombreServicio);
+            divServicio.appendChild(precioServicio);
+            console.log(divServicio);
         });
     }catch(error){
         console.log(error);
